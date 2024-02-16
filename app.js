29755508f13859e8d3ebf9a12e10d27c002be01f
asif12018 <asif.request@gmail.com>
2024-02-16 18:24:17 +0600

    first commit

--- app.js
+++ app.js
@@ -4,68 +4,91 @@ let ejs = require('ejs');
 const getDate = require('./date');
 const app = express();
 const date = require(__dirname+'/date.js');
+const mongoose = require('mongoose')
 
 
 
-let items = [];
-let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+// 1. create a data base
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+// 2. create new schema
+
+const itemsSchema = {
+    name: String
+}
+
+// 3. create new model
+
+const Item = mongoose.model('Item',
+itemsSchema
+);
+
+// create same data
+
+const item1 = new Item({
+    name: 'item number 1 is here'
+});
+
+
+
+
+// array for data base
+
+const defaultItems = [item1];
+
+//insert to database
+Item.insertMany(defaultItems);
+
 app.get('/', function (req, res) {
-    
-    
-    // old code:
-    // switch (currentDay) {
-    //     case 0:
-    //         day = 'Sunday';
-    //         break;
 
-    //     case 1:
-    //         day = 'Monday';
-    //         break;
-
-    //     case 2:
-    //         day = 'tuesday';
-    //         break;
-
-
-    //     case 3:
-    //         day = 'Wednesday';
-    //         break;
-
-    //     case 4:
-    //         day = 'Thursday';
-    //         break;
-
-    //     case 5:
-    //         day = 'Friday';
-    //         break;
-
-    //     default:
-    //         day: 'Saturday'
-    //         break;
-    // }
-    // updated and better code down here:
+    Item.find({})
+    .then((foundItems) => {
+        if(foundItems.length === 0){
+            res.redirect('/')
+        }else{
+             
+             res.render('index', { listTitle: 'Today', newItems: foundItems });
+        }
+    });
 
     
-    let day = date.getDate();
-    res.render('index', { listTitle: day, newItems: items });
 });
 
 app.post('/',function(req,res){
-    let item = req.body.newItem;
-    if(req.body.list=== 'work'){
-        workItems.push(item);
-        res.redirect('/work');
-    }else{
-        items.push(item);
-        res.redirect('/');
-    }
+    let itemName = req.body.newItem;
+    
+    const item = new Item({
+        name:itemName
+    });
+
+    item.save();
+    res.redirect('/');
     
     
-})
+});
+
+app.post('/delete',function(req,res){
+    const checkedItemId = req.body.checkbox;
+    Item.findByIdAndDelete(checkedItemId)
+    .then(deletedProduct => {
+        if (!deletedProduct) {
+            console.log('No product found with that ID');
+        } else {
+            console.log('Product deleted:', deletedProduct);
+        }
+    })
+    .catch(err => {
+        console.error('Error deleting product:', err);
+    });
+      
+    res.redirect('/')
+});
+
 app.listen(3000, function () {
     console.log('server is running in port 3000 ');
 });
